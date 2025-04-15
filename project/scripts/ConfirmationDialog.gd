d1ddc0c61770a992c9fa10450f7c320decf328e1
Roman Dubrov <otumno@gmail.com>
2025-04-15 13:55:35 +0300

    Stable version

--- project/scripts/ConfirmationDialog.gd
+++ project/scripts/ConfirmationDialog.gd
@@ -1,22 +1,28 @@
 extends ConfirmationDialog
 
-signal confirmed
+# Переименованный сигнал, чтобы избежать конфликта с родительским классом
+signal confirmation_accepted
 
 func _ready() -> void:
 	# Настройка диалога
 	dialog_autowrap = true
 	get_label().horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	
+	# Настройка текста кнопок
+	get_ok_button().text = "Подтвердить"
+	get_cancel_button().text = "Отмена"
+	
 	# Подключаем сигналы кнопок
 	confirmed.connect(_on_confirmed)
-	cancel_button.pressed.connect(_on_cancel_pressed)
+	get_cancel_button().pressed.connect(_on_cancel_pressed)
 
 func show_confirmation(message: String) -> void:
 	dialog_text = message
 	popup_centered()
 
 func _on_confirmed() -> void:
-	confirmed.emit()
+	# Испускаем наш кастомный сигнал
+	confirmation_accepted.emit()
 	hide()
 
 func _on_cancel_pressed() -> void:
